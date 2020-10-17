50ffddde468228049eddb3f49b03dc6bb17bc75b
João Carlos Junior <56278963+joaojuniorpy@users.noreply.github.com>
2020-10-16 21:13:05 -0300

    Update test.py

--- test.py
+++ test.py
@@ -1,18 +1,34 @@
-def primos():
-	anterior = 0
-	proximo = 0
-	lista = []
 
-	while(proximo < 1000000000000000000003):
-		lista.append(proximo)
-		proximo = proximo + anterior
-		anterior = proximo - anterior
-		if(proximo == 0):
-			proximo = proximo + 1
+import os
+from flask import Flask, jsonify, request
+from math import sqrt
 
-	print(lista)
-	print("Foram printados",len(lista),"numerais")
+app = Flask(__name__)
 
+@app.route('/')
+def nao_entre_em_panico():
+	limite = 100
+	
 
-primos()
+	p = 1
+	numero = 3
+	primos = "2,"
+	
+	while p < limite:
+		ehprimo = 1
+		for i in range(2, numero):
+			if numero % i == 0:
+				ehprimo = 0 
+				break
+		if (ehprimo):
+			primos = primos + str(numero) + ","
+			p += 1
+			if(p % 10 == 0):
+				primos = primos + "<br>"
+		numero += 1
+	return primos
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
 
